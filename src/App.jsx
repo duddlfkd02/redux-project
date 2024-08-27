daf67b202854c2551af67d3937465b9556f5aac3
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-08-27 14:15:37 +0900

    feat: dispatch 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const App = () => {
   const counterReducer = useSelector((state) => {
     return state.counter;
   });
-  console.log("🚀 ~ App ~ counterReducer:", counterReducer);
+  // console.log("🚀 ~ App ~ counterReducer:", counterReducer);
+  const dispatch = useDispatch();
 
-  return <div></div>;
+  return (
+    <div>
+      {counterReducer.number}
+      <button
+        onClick={() => {
+          // dispatch가 reducer에게 action 객체 type값을 넘겨줌
+          dispatch({ type: "PLUS_ONE" });
+        }}
+      >
+        +1
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "MINUS_ONE" });
+        }}
+      >
+        -1
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -5,7 +5,18 @@ const initialState = {
 
 // 리듀서 함수
 const counter = (state = initialState, action) => {
-  switch (action.type) {
+  console.log("🚀 ~ counter ~ action:", action);
+  switch (
+    action.type // dispatch에게 넘겨받은 type ⭐️ 이 type으로 state값을 변경하기 때문에 action의 type이 중요하다.
+  ) {
+    case "PLUS_ONE":
+      return {
+        number: state.number + 1,
+      };
+    case "MINUS_ONE":
+      return {
+        number: state.number - 1,
+      };
     default:
       return state;
   }
